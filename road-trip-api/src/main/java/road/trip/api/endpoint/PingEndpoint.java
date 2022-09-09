93705fa46591e1ca38802825cf558ca80bbfa61b
Harm <harm_drenth1@baylor.edu>
2022-09-09 16:38:52 -0500

    [DEPLOY-DB]

--- road-trip-api/src/main/java/road/trip/api/endpoint/PingEndpoint.java
+++ road-trip-api/src/main/java/road/trip/api/endpoint/PingEndpoint.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-@RestController 
+@RestController
 public class PingEndpoint {
     @GetMapping("/ping")
     public String ping() {
