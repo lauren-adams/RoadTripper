59bd9b706df62013b45dee5a4252c3dff1ee0d64
Harmond Drenth <harm_drenth1@baylor.edu>
2022-09-19 22:44:30 +0000

    Update road-trip-api/src/main/java/road/trip/api/endpoint/RegisterEndpoint.java

--- road-trip-api/src/main/java/road/trip/api/endpoint/RegisterEndpoint.java
+++ road-trip-api/src/main/java/road/trip/api/endpoint/RegisterEndpoint.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RegisterEndpoint {
     @GetMapping("/register")
-    public String ping() {
+    public String register() {
         return "register!";
     }
 
