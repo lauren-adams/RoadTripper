c1a3eb2f549ce56ac50f61ba7c53ab88a6831cdb
Ryan Huntington <ryan_huntington1@baylor.edu>
2022-09-19 13:04:17 -0500

    test to create pages using react

--- road-trip-frontend/src/pages/index.js
+++ road-trip-frontend/src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import RouterConfig from './Route.js'
 import Link from '@material-ui/core/Link';
 
 require('dotenv').config();
@@ -7,7 +8,9 @@ function HomePage() {
     return (
         <div>
             This is the home page!
+            <RouterConfig />
         </div>
+        
     )
 }
 
