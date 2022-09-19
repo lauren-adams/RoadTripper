28acf2618c83c7e065dc5f9e05dd3f49f0b33bfc
Ryan Huntington <ryan_huntington1@baylor.edu>
2022-09-19 16:01:56 -0500

    [BUILD-FRONTEND] i hope it works, along with some routing changes

---
+++ road-trip-frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div>
+            This is the home page!
+        </div>
+
+    )
+}
+
+export default HomePage
--- road-trip-frontend/src/pages/Route.js
+++ road-trip-frontend/src/pages/Route.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Login from "./login.js"
-import Home from './index'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './login'
+import Home from './HomePage'
 
 
 
@@ -10,12 +10,12 @@ export default function RouterConfig() {
     /*
     Here we define the route path and its corresponding components
     */
-    //FIXME need to change it so index.js is not the home page, just that way right now
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route exact path="/" element={<Home />} />
-                <Route exact path="/loginpage" element={<Login />} />
+                <Route path="/loginpage" element={<Login />} />
 
             </Routes>
         </BrowserRouter>
