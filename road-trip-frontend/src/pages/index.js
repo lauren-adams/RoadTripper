fca65df29a1ef28640a84ea58d4816bf0fd58d9b
Ryan Huntington <ryan_huntington1@baylor.edu>
2022-09-19 13:21:40 -0500

    pushing again

--- road-trip-frontend/src/pages/index.js
+++ road-trip-frontend/src/pages/index.js
@@ -4,13 +4,15 @@ import Link from '@material-ui/core/Link';
 
 require('dotenv').config();
 
+//FIXME pls dont let this be the homepage, we actually need a homepage class im just lazy right now
+//ill fix in a bit, we have some time.
 function HomePage() {
     return (
         <div>
             This is the home page!
             <RouterConfig />
         </div>
-        
+
     )
 }
 
