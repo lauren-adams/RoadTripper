9a14b5e980de29e36f1ef284569184d55514855c
Ruiqi Zhao <ricky_zhao1@baylor.edu>
2022-10-08 01:30:46 -0400

    new Add a two function for print trip name, hope this will work, but will need to implement other with this

--- road-trip-frontend/src/App.js
+++ road-trip-frontend/src/App.js
@@ -4,6 +4,9 @@ import Maps from "./components/Maps";
 import Trip from "./components/Trip";
 import Home from "./components/Home";
 import { Route, Switch, Redirect, NavLink } from "react-router-dom";
+import TripList from "./components/TripList";
+
+const tripArr = ['trip1', 'trip2', 'trip3'];  // for test use
 
 function App() {
 
@@ -12,9 +15,9 @@ function App() {
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/signup" component={SignUp} />
-          <Route path="/maps" component={Maps} />
-          <Route path="/view-trips" component={Trip} />
-          <Route path="/home" component={Home} />
+        <Route path="/maps" component={Maps} />
+        <Route path="/view-trips" component={TripList(tripArr)} />    {/*hard coded*/}
+        <Route path="/home" component={Home} />
         <Redirect from="/" to="/home" />
       </Switch>
     </>
