0e97e35acc4b9b63a536516c7fb2f006996eb97a
Ryan <ryan_huntington1@baylor.edu>
2022-11-06 15:18:50 -0600

    initial preference testing

--- road-trip-api/src/main/java/road/trip/api/preference/PreferenceService.java
+++ road-trip-api/src/main/java/road/trip/api/preference/PreferenceService.java
@@ -3,18 +3,21 @@ package road.trip.api.preference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
 public class PreferenceService {
+
     @Autowired
     PreferenceRepository preferenceRepository;
 
+
     public List<Preference> findPreferenceByUser(String userId){ return preferenceRepository.findByUserId(userId); }
-
+    @Transactional
     public void deletePreference(Long id){ preferenceRepository.deleteById(id); }
-
+    @Transactional
     public Preference savePreference(Preference pref){ return preferenceRepository.save(pref); }
-
+    @Transactional
     public void deletePrefByUserId(String userId){ preferenceRepository.deleteAllByUserId(userId); }
 }
