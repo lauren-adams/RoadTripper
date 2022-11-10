de704919e97f74ce4e482f6bc35038af914ef54a
Harmond Drenth <harm_drenth1@baylor.edu>
2022-11-10 20:31:29 +0000

    [BUILD-FRONTEND]

--- road-trip-frontend/src/components/RenderMap.js
+++ road-trip-frontend/src/components/RenderMap.js
@@ -2,6 +2,13 @@ import React, {Component} from "react";
 import {Map, Marker, GoogleApiWrapper} from "google-maps-react"
 
 class App extends Component {
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            console.log("Latitude is :", position.coords.latitude);
+            console.log("Longitude is :", position.coords.longitude);
+        });
+    }
+
     render() {
         const style = {
             width: "1000px",
@@ -14,8 +21,8 @@ class App extends Component {
                     google={this.props.google}
                     zoom={15}
                     initialCenter={{
-                        lat: 31.5500894,
-                        lng: -97.1157287
+                        lat: 31.5501,
+                        lng: -97.1135
                     }}
                     style={style}/>
                 <Marker/>
