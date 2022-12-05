63011222113b7b05e6357c02d44b0eba80c7329a
hkaase <livin2chill@yahoo.com>
2022-12-04 18:25:15 -0600

    [BUILD-FRONTEND]

--- road-trip-api/src/main/java/road/trip/api/SecurityConfigurer.java
+++ road-trip-api/src/main/java/road/trip/api/SecurityConfigurer.java
@@ -29,7 +29,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.cors().and().csrf().disable();
     }
-
+ 
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
