02253d4ab1ce031ca59f8a1ae0ef23c76b9e1af0
hkaase <livin2chill@yahoo.com>
2022-12-04 20:08:43 -0600

    [BUILD-API]

--- road-trip-api/src/main/java/road/trip/api/SecurityConfigurer.java
+++ road-trip-api/src/main/java/road/trip/api/SecurityConfigurer.java
@@ -1,5 +1,5 @@
 package road.trip.api;
-import road.trip.api.endpoint.RequestFilter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,11 +7,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import road.trip.api.endpoint.RequestFilter;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
@@ -28,14 +28,17 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/**", "/trip/*","/authenticate", "/user/getPassword*", "/user/*/preferences", "/user/saveUser*", "***", "/user/*").permitAll().anyRequest().authenticated()
-                .and()
-                .exceptionHandling().and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        httpSecurity.addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
-        httpSecurity.cors().and().csrf().disable();
+        //httpSecurity.csrf().disable().cors().and()
+                //.authorizeRequests().antMatchers("/**", "/trip/*","/authenticate", "/user/getPassword*", "/user/*/preferences", "/user/saveUser*", "***", "/user/*").permitAll().anyRequest().authenticated()
+                //.and()
+                //.exceptionHandling().and().sessionManagement()
+                //.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //httpSecurity.addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
 
+        httpSecurity
+                .addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .anyRequest().authenticated();
     }
 
     @Override
